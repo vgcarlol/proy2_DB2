92dc0b1c6cb11552be9d6606f576c69eca4cda7d
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2025-05-06 00:57:23 -0600

    swagger

--- restaurante_api/main.py
+++ restaurante_api/main.py
@@ -1,25 +1,26 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from routes import restaurantes, usuarios, articulos_menu, ordenes, resenas
+from app.routes.usuarios import router as usuarios_router
+from app.routes.restaurantes import router as restaurantes_router
+from app.routes.articulos_menu import router as menu_router
+from app.routes.ordenes import router as ordenes_router
+from app.routes.resenas import router as resenas_router
+from app.index_setup import crear_indices  # Importar función de índices
 
+app = FastAPI(title="API Restaurante - Proyecto 2")
 
-app = FastAPI(title="Sistema de Gestión de Pedidos y Reseñas")
-
+# Ruta de prueba de conexión
 @app.get("/")
 async def root():
-    return {"mensaje": "Conexión activa con MongoDB Atlas"}
+    return {"mensaje": "API de Restaurante conectada a MongoDB Atlas"}
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # O especifica ["http://127.0.0.1:5500"] si quieres más seguro
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# Registrar todos los routers
+app.include_router(usuarios_router)
+app.include_router(restaurantes_router)
+app.include_router(menu_router)
+app.include_router(ordenes_router)
+app.include_router(resenas_router)
 
-# Incluir rutas
-app.include_router(restaurantes.router)
-app.include_router(usuarios.router)
-app.include_router(articulos_menu.router)
-app.include_router(ordenes.router)
-app.include_router(resenas.router)
+# Crear índices al iniciar
+@app.on_event("startup")
+async def startup_event():
+    await crear_indices()
